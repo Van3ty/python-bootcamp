5983881f94cc762f0f675972fa64c1c0114873fb
van3ty <homiesindabank@gmail.com>
2026-06-22 13:52:01 +0300

    Updated the api inventory

--- python-phase1/api_inventory.py
+++ python-phase1/api_inventory.py
@@ -7,11 +7,13 @@ response = requests.get("https://jsonplaceholder.typicode.com/posts")
 if response.status_code == 200:
     logging.info("Request successful")
     data = response.json()
-    for post in data[:5]:
-        print(post["title"])
-        print(post["id"])
 else:
     logging.error("Failed to retrieve data")
 
-with open("posts.json", "w") as file:
-    json.dump(data, file,)
+with open("posts.json", "r") as file:
+    data = json.load(file)
+
+for post in data:
+    print(post["title"])
+    print(post["body"])
+    print("-" * 40)
