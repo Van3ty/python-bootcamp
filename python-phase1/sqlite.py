d341a1715ead24c54a0fd57e9a2833dc61c5cabe
van3ty <homiesindabank@gmail.com>
2026-07-01 16:48:12 +0300

    Added some changes to the SQLite file

--- python-phase1/sqlite.py
+++ python-phase1/sqlite.py
@@ -13,11 +13,33 @@ cursor.execute("""
 
 
 
+cursor.execute("""
+               INSERT INTO books VALUES(1, 'Gatsby', 15.99)
+               """)
+
+cursor.execute("""
+               INSERT INTO books VALUES(2, '1984', 10.99)
+               """)
+
+cursor.execute("""
+               INSERT INTO books VALUES(3, 'To Kill a Mockingbird', 12.99)
+               """)
+
+cursor.execute("""
+               INSERT INTO books VALUES(4, 'Pride and Prejudice', 9.99)
+               """)
+
+cursor.execute("""
+               INSERT INTO books VALUES(5, 'The Catcher in the Rye', 14.99)
+               """)
+
+cursor.execute("""
+               INSERT INTO books VALUES(6, 'The Hobbit', 11.99)
+               """)
 
 conn.commit()
 
 cursor.execute("SELECT * FROM books")
 rows = cursor.fetchall()
 print(rows)
-
 
